1eaf7d15c25fd4c6a70c36beb50b2db498a99536
Elizabeth Jamison <ejamison3@gmail.com>
2021-02-11 20:46:12 -0500

    create functions in crud.py

--- crud.py
+++ crud.py
@@ -4,6 +4,11 @@ May split this into multiple files in future"""
 
 from model import Book, Category, Author, Tag, User, Ratingdb
 
+# CURRENT PROBLEMS
+# 1. No way to set has_read attribute in users_books table
+# 2. No way to create link between users_books and books_tags table. should this even be a thing? 
+#    Yes, it should but I gotta think it through
+
 # categories and tags records are created in db_setup.py file. This is a one time creation. 
 
 def create_book(
@@ -47,8 +52,7 @@ def create_book(
         print(f'create relationship to users table - NEED TO IMPLEMENT')
 
     return temp_book
-
-    
+  
 
 # not all authors have first names
 def create_author(lname, fname=None):
@@ -64,9 +68,18 @@ def create_author(lname, fname=None):
 def create_books_authors_relationship(book_record, author_record):
     """Create relationship between books and authors tables"""
 
+    book_record.authors.append(author_record)
+    db.session.commit() # I don't believe I need to add?
 
-def create_books_tags_relationship():
-    """Create relationship between books and tags tables"""
+
+def create_books_tags_relationship(book_record, tag_record):
+    """Create relationship between books and tags tables
+    
+    arguments are single book and single author record
+        If wanting to add multiple,need to call function multiple times"""
+    
+    book_record.tags.append(tag_record)
+    db.session.commit()
 
 
 def create_user(name, pw):
@@ -83,9 +96,28 @@ def create_user(name, pw):
     return temp_user
 
 
-def create_users_books_relationship():
+def create_users_books_tags_relationship(user_book_record, book_tag_record):
+    """Create relationship between users_books table and books_tags table"""
+
+    user_book_record.book_tags.append(book_tag_record)
+    db.session.commit()
+
+
+def create_users_books_relationship(user_record, book_record):
     """Create relationship between users and books tables"""
 
+    user_record.books.append(book_record)
+    db.session.commit()
 
-def create_rating():
+
+def create_rating(score, user_record, book_record, description=None):
     """Create rating record and tie to users and books tables"""
+
+    temp_rating = Rating(score=score, description=description)
+    db.session.add(temp_rating)
+    db.commit()     # do now to create ratings.id?
+
+    # create relationships to users and books tables
+    user_record.ratings.append(temp_rating)
+    book_record.ratings.append(temp_rating)
+
